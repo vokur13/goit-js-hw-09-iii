d3333026c4dcc3b2a4de4f12dbd85c92d5a8e17e
vokur13 <103522917+vokur13@users.noreply.github.com>
2022-08-31 10:57:10 +0000

    Deploying to gh-pages from @ vokur13/goit-js-hw-09-iii@5df230024ed55774a9c8c0026e9b4c194845f493 🚀

---
+++ 01-color-switcher.e622b5d5.js
@@ -0,0 +1,2 @@
+!function(){let t=null;const n={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};n.btnStart.addEventListener("click",(function(){n.btnStart.disabled=!0,t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),n.btnStop.addEventListener("click",(function(){clearInterval(t),n.btnStart.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.e622b5d5.js.map
